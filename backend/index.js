b006322eabc07e41ea38221d0ff1f2048ace7115
vivekkatkar <katkarvivek0@gmail.com>
2025-04-15 16:41:41 +0530

    Final backend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,56 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import  mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+const dbURI = process.env.DATABASE_URL;
+
+console.log(dbURI);
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number
+});
+const User = mongoose.model('User', userSchema);
+
+const seedUsers = async () => {
+  const users = [
+    { name: 'Alice', email: 'alice@example.com', age: 30 },
+    { name: 'Bob', email: 'bob@example.com', age: 25 },
+    { name: 'Charlie', email: 'charlie@example.com', age: 35 }
+  ];
+
+  await User.insertMany(users);
+  console.log('Users seeded');
+};
+
+seedUsers();
 
 app.get("/", (req, res) => {
-    res.send("Hello from server");
+    res.status(200).json({
+        message : "Welcome to server"
+    })
 })
 
-app.get("/get-user", (req, res) => {
-    const user = {
-        name : "vivek",
-        mobile : 9356907874
-    };
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).send('Error retrieving users');
+    }
+  });
 
-    res.status(200).json({data : user});
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
-
-app.listen(3000, ()=>{
-    console.log("app is running on port 3000");
-})
